938609ff9e965c3d4a7a922ecb83715e764a2419
Fernando Hattori <fhattori@gmail.com>
2022-12-08 21:43:43 -0300

    Primeiro commit para o Serviço de Dados Projeto do Spring Initializr ainda vazio, onde será desenvolvido o Serviço de Dados

---
+++ dados/src/test/java/com/btgpactual/desafio/dados/DadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btgpactual.desafio.dados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
